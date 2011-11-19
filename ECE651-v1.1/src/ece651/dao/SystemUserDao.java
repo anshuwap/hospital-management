8e08273f1af70ea1cb4a0931f14e82e4ef306555
dannyjiang85 <dannyjiang85@gmail.com>
2011-11-19 04:38:43 +0000

    Initial check-in for setting system user struct and add CreateSystemUser action

--- ECE651-v1.1/src/ece651/dao/SystemUserDao.java
+++ ECE651-v1.1/src/ece651/dao/SystemUserDao.java
@@ -5,6 +5,7 @@ import ece651.dao.DAOException;
 
 public interface SystemUserDao {
 	public void cleanup();
+	public void saveUser (SystemUser user) throws DAOException;
 	public SystemUser searchUserBySystemUserId (int SystemUserId ) throws DAOException;
 	public SystemUser searchUserByUsername ( String Username ) throws DAOException;
 }
